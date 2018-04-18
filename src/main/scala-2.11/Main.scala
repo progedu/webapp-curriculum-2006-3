9d7477cd104aa2c151bceaaf970b4c5cdf97fabe
apprec100 <apprec100@outlook.com>
2018-04-18 19:12:28 +0900

    split メソッドを実装した

--- src/main/scala-2.11/Main.scala
+++ src/main/scala-2.11/Main.scala
@@ -1,3 +1,4 @@
 object Main {
-  def split[A](n: Int, list: List[A]): (List[A], List[A]) = ???
+  def split[A](n: Int, list: List[A]): (List[A], List[A]) =
+    (list.take(n), list.takeRight(list.size - n))
 }
