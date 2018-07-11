af0f45e667bb17def2eaac9c209b57100ceb6769
nonoike <12398740+nonoike@users.noreply.github.com>
2018-07-11 19:44:25 +0900

    挑戦上級 split メソッドを実装

--- src/main/scala-2.11/Main.scala
+++ src/main/scala-2.11/Main.scala
@@ -1,3 +1,13 @@
 object Main {
-  def split[A](n: Int, list: List[A]): (List[A], List[A]) = ???
+  def split[A](n: Int, list: List[A]): (List[A], List[A]) = {
+    import scala.annotation.tailrec
+    @tailrec
+    def loop(i: Int, left: List[A], right: List[A]): (List[A], List[A]) = i match {
+      case 0 => (left.reverse, right)
+      case _ if right.isEmpty => (left.reverse, right)
+      case _ => loop(i - 1, right.head :: left, right.tail)
+    }
+
+    loop(n, Nil, list)
+  }
 }
