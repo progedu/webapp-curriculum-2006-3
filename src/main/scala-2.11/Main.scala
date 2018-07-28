2c3287d64affae60b518e9a50918b10a7eea14cb
yukikoya <36853872+yukikoya@users.noreply.github.com>
2018-07-28 22:58:54 +0900

    split実装

--- src/main/scala-2.11/Main.scala
+++ src/main/scala-2.11/Main.scala
@@ -1,3 +1,16 @@
 object Main {
-  def split[A](n: Int, list: List[A]): (List[A], List[A]) = ???
+  def split[A](n: Int, list: List[A]): (List[A], List[A]) = list match {
+    case Nil => (Nil,Nil)
+    case _  if n >= 0 => (list.take(n),list.takeRight(list.length-n))
+  }
+  def split2[A](n:Int, list:List[A]):(List[A],List[A]) = {
+    def splitRec[A](index:Int,take:List[A],lest:List[A]):(List[A], List[A]) = {
+      (index,lest) match {
+        case (_,Nil) => (take.reverse,Nil)
+        case (0,list) => (take.reverse,list)
+        case (i,x::xs) =>splitRec(i-1,x::take,xs)
+      }
+    }
+    splitRec(n, Nil, list)
+  }
 }
