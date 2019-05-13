3e1a13688e2a386228b5ef62a0b9a28a9ee12d0c
syo <M.youta.kwg@gmail.com>
2019-05-14 00:31:22 +0900

    splitを実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,7 @@
 object Main {
-  def split[A](n: Int, list: List[A]): (List[A], List[A]) = ???
+  def split[A](n: Int, list: List[A]): (List[A], List[A]) = {
+    list.zipWithIndex.foldLeft((Nil: List[A], Nil: List[A])) { case ((left, right), (e, i)) =>
+    if (i < n) (left :+ e, right) else (left, right :+ e)
+    }
+  }
 }
