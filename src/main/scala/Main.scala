974033d06d97e0de0eef79ec84b361fc8060634d
YuuTakimotoY <44459676+YuuTakimotoY@users.noreply.github.com>
2020-06-19 00:04:55 +0900

    splitの実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,11 @@
 object Main {
-  def split[A](n: Int, list: List[A]): (List[A], List[A]) = ???
+  def split[A](n: Int, list: List[A]): (List[A], List[A]) = {
+    def splitRec(index: Int, take: List[A], rest: List[A]): (List[A], List[A]) =
+      (index, rest) match {
+        case (_, Nil) => (take.reverse, Nil)
+        case (0, list) => (take.reverse, rest)
+        case (i, x::xs) => splitRec(i - 1, x::take, xs)
+      }
+      splitRec(n, Nil, list)
+    }
 }
