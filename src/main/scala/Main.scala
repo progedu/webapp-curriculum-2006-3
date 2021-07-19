4d0778d7bf02d7cfe6e8f24421c883037844f0ee
gomi369 <assassincreed0461-gta005@outlook.jp>
2021-07-20 04:37:28 +0900

    テスト出来ました。

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,14 @@
+import scala.annotation.tailrec
+
 object Main {
-  def split[A](n: Int, list: List[A]): (List[A], List[A]) = ???
+  def split[A](n: Int, list: List[A]): (List[A], List[A]) = {
+    @tailrec
+    def splitR(curR: Int, curL: List[A], pre: List[A]): (List[A], List[A]) =
+      (curR, curL) match {
+        case (_, Nil) => (pre.reverse, Nil)
+        case (0, list) => (pre.reverse, list)
+        case (n, h :: tail) => splitR(n - 1, tail, h :: pre)
+      }
+    splitR(n, list, Nil)
+  }
 }
