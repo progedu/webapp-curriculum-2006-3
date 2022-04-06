6acc720acd5a7d2cff2b31f392df3c2eb09c5bfd
Kazuki Nitta <nittakdub@gmail.com>
2022-04-06 23:17:19 +0900

    splitの実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,13 @@
 object Main {
-  def split[A](n: Int, list: List[A]): (List[A], List[A]) = ???
+  def split[A](n: Int, list: List[A]): (List[A], List[A]) = list match {
+    case Nil => (Nil, Nil)
+    case head :: tl => if(n == 1)
+    {
+      (List(head), tl)
+    } else {
+      val leftHalf = split(n-1, tl)._1
+      val rightHalf = split(n-1, tl)._2
+      ((head :: leftHalf), rightHalf)
+    }
+  }
 }
