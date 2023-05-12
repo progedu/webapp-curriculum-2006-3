74b9f918946ca2da4c0db3408d97782b842dc41b
hosh294 <132256088+hosh294@users.noreply.github.com>
2023-05-12 17:59:31 +0900

    split[A](Int, List[A]): (List[A], List[A])の実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,16 @@
+import scala.annotation.tailrec
+
 object Main {
-  def split[A](n: Int, list: List[A]): (List[A], List[A]) = ???
+  def split[A](n: Int, list: List[A]): (List[A], List[A]) = {
+    if (n > list.length) {
+      return (Nil, list)
+    }
+    @tailrec
+    def splitInner(i: Int, acc: List[A], list: List[A]): (List[A], List[A]) = i match {
+      case 0 => (Nil, list)
+      case 1 => (acc :+ list.head, list.tail)
+      case _ => splitInner(i - 1, acc :+ list.head, list.tail)
+    }
+    splitInner(n, Nil, list)
+  }
 }
